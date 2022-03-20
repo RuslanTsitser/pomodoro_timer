e9a97867f7be23c4d4be4f1d39058105a3370481
Ruslan Tsitser <cicer_ruslan@mail.ru>
2022-03-20 18:57:03 +0300

    added timer button

---
+++ lib/presentation/widgets/timer_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class TimerButton extends StatefulWidget {
+  const TimerButton({
+    Key? key,
+    required this.onPressed,
+    this.started = false,
+  }) : super(key: key);
+  final void Function() onPressed;
+  final bool started;
+
+  @override
+  State<TimerButton> createState() => _TimerButtonState();
+}
+
+class _TimerButtonState extends State<TimerButton> {
+  late bool started;
+
+  @override
+  void initState() {
+    started = widget.started;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          fixedSize: const Size(181.0, 48.0),
+        ),
+        child: Text(started ? 'STOP' : 'FOCUS'),
+        onPressed: () {
+          if (started) {
+            setState(() {
+              started = false;
+            });
+          } else {
+            setState(() {
+              started = true;
+            });
+          }
+          widget.onPressed();
+        },
+      ),
+    );
+  }
+}
