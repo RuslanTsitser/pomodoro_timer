64a9e320247ffa03183c6ab05272df9605c625df
Ruslan Tsitser <cicer_ruslan@mail.ru>
2022-03-21 22:14:16 +0300

    timer button to stateless

--- lib/presentation/widgets/timer_button.dart
+++ lib/presentation/widgets/timer_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class TimerButton extends StatefulWidget {
+class TimerButton extends StatelessWidget {
   const TimerButton({
     Key? key,
     required this.onPressed,
@@ -10,37 +10,13 @@ class TimerButton extends StatefulWidget {
   final bool started;
 
   @override
-  State<TimerButton> createState() => _TimerButtonState();
-}
-
-class _TimerButtonState extends State<TimerButton> {
-  late bool started;
-
-  @override
-  void initState() {
-    started = widget.started;
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return OutlinedButton(
       style: OutlinedButton.styleFrom(
         fixedSize: const Size(181.0, 48.0),
       ),
       child: Text(started ? 'STOP' : 'FOCUS'),
-      onPressed: () {
-        if (started) {
-          setState(() {
-            started = false;
-          });
-        } else {
-          setState(() {
-            started = true;
-          });
-        }
-        widget.onPressed();
-      },
+      onPressed: onPressed,
     );
   }
 }
