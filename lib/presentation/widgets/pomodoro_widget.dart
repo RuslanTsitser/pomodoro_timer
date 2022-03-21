de86eb0908af6039aaf4e5406c0aeda8e30378db
Ruslan Tsitser <cicer_ruslan@mail.ru>
2022-03-21 22:14:27 +0300

    widget fixes

--- lib/presentation/widgets/pomodoro_widget.dart
+++ lib/presentation/widgets/pomodoro_widget.dart
@@ -4,15 +4,20 @@ class PomodoroWidget extends StatelessWidget {
   const PomodoroWidget({
     Key? key,
     required this.pomodoroNumber,
+    required this.percentage,
   }) : super(key: key);
   final int pomodoroNumber;
+  final double percentage;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    double _currentPomodoroNumber = pomodoroNumber * percentage / 100;
+    return Container(
+      padding: const EdgeInsets.only(left: 10),
       height: 40,
       width: pomodoroNumber * 60,
       child: ListView.separated(
+        padding: EdgeInsets.zero,
         separatorBuilder: (context, index) {
           return const SizedBox(
             width: 20,
@@ -21,6 +26,21 @@ class PomodoroWidget extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: pomodoroNumber,
         itemBuilder: (context, index) {
+          if (index >= _currentPomodoroNumber) {
+            return Image.asset(
+              'assets/icons/pomodoro_empty.png',
+              width: 40,
+              height: 40,
+            );
+          }
+          if (index + 0.5 >= _currentPomodoroNumber) {
+            return Image.asset(
+              'assets/icons/pomodoro_15.png',
+              width: 40,
+              height: 40,
+            );
+          }
+
           return Image.asset(
             'assets/icons/pomodoro.png',
             width: 40,
